634a4e0b42916162a22928c6e2311676477400bf
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-06-11 07:19:51 -0500

    added starter pages and router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Teacher to Techie</h1>
+      <HomePage />
+      <AboutPage />
+      <ArticlesList />
+      <ArticlePage />
     </div>
   );
 }
