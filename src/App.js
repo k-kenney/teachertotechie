21a5f037298a42a44064b32a67a97ab30029108f
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-06-11 12:04:02 -0500

    added Links from react router dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import './App.css';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ArticlePage from './pages/ArticlePage';
-import ArticlesList from './pages/ArticlesList';
+import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teacher to Techie</h1>
-      <HomePage />
-      <AboutPage />
-      <ArticlesList />
-      <ArticlePage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <h1>Teacher to Techie</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesList />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
