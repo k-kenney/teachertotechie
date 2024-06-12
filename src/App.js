974cab537e860fa372d6c15fe9cf549e80b4b99d
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-06-12 08:42:18 -0500

    created not found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import NavBar from "./NavBar";
 
 function App() {
@@ -11,13 +12,13 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <NavBar />
-        <h1>Teacher to Techie</h1>
         <div id="page-body">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/articles" element={<ArticlesListPage />} />
             <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
       </div>
